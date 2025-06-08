2d94986c3903d199adc899275de99d35b8bbe744
ren <karenjw2003@gmail.com>
2025-06-08 17:11:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import random
 
+elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+panjang_password = int(input("Masukkan panjang password: "))
+password = ""
+for i in range(panjang_password):
+    password += random.choice(elements)
+print("Password Anda adalah: ", password)
+print("Selamat, password anda telah dibuat!")
